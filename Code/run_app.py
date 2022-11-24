cc223c8a53225f0c034a70f8444359ae0e691955
Shaib <Shaitemp88@gmail.com>
2022-11-24 04:59:57 -0800

    code fix1

--- Code/run_app.py
+++ Code/run_app.py
@@ -22,30 +22,30 @@ print ("=====================")
 
 while True:
     try:
-      ec2 = boto3.resource('ec2', region_name=REGION, aws_access_key_id=ACCESS_ID,
-                           aws_secret_access_key=ACCESS_KEY)
-      instances = ec2.instances.filter(
-          Filters=[
-              {
-                  'Name': 'tag:Name',
-                  'Values': [
-                      "instance-state-code",
-                      "tag:k8s.io/role/master"
-                  ]
-              },
-              {
-                  'Name': 'instance-state-code',
-                  'Values': [
-                      "16"
-                  ]
-              }
-          ]
-      )
-      for instance in instances:
-          for tag in instance.tags:
-              print (tag)
-          print ("instance id: ", instance.id)
+        ec2 = boto3.resource('ec2', region_name=REGION,
+                     aws_access_key_id=ACCESS_ID, aws_secret_access_key=ACCESS_KEY)
+        instances = ec2.instances.filter(
+            Filters=[
+                {
+                    'Name': 'tag:Name',
+                    'Values': [
+                        "instance-state-code",
+                        "tag:k8s.io/role/master"
+                    ]
+                },
+                {
+                    'Name': 'instance-state-code',
+                    'Values': [
+                        "16"
+                    ]
+                }
+            ]
+        )
+        for instance in instances:
+            for tag in instance.tags:
+                print (tag)
+            print ("instance id: ", instance.id)
     except:
-      print ("=Exception=")
+        print ("=Exception=")
     print ("Done - waiting for next interval")
     time.sleep(INTERVAL)
