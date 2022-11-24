ec4f210bb183a0e618d6a2c2f38bd582d0e45c53
Shaib <Shaitemp88@gmail.com>
2022-11-24 04:52:55 -0800

    fix

--- Code/run_app.py
+++ Code/run_app.py
@@ -12,37 +12,40 @@ ACCESS_KEY = os.environ['aws_secret_access_key']
 REGION = os.environ['region']
 INTERVAL = int(os.environ["INTERVAL"])
 
-#print ("=                   =")
-#print ("Envoronment Variable:")
-#print (ACCESS_ID)
-#print (ACCESS_KEY)
-#print (REGION)
-#print (INTERVAL)
-#print ("=====================")
+print ("=                   =")
+print ("Envoronment Variable:")
+print (ACCESS_ID)
+print (ACCESS_KEY)
+print (REGION)
+print (INTERVAL)
+print ("=====================")
 
 while True:
-    ec2 = boto3.resource('ec2', region_name=REGION, aws_access_key_id=ACCESS_ID,
-                         aws_secret_access_key=ACCESS_KEY)
-    instances = ec2.instances.filter(
-        Filters=[
-            {
-                'Name': 'tag:Name',
-                'Values': [
-                    "instance-state-code",
-                    "tag:k8s.io/role/master"
-                ]
-            },
-            {
-                'Name': 'instance-state-code',
-                'Values': [
-                    "16"
-                ]
-            }
-        ]
-    )
-    for instance in instances:
-        for tag in instance.tags:
-            print (tag)
-        print ("instance id: ", instance.id)
+    try:
+      ec2 = boto3.resource('ec2', region_name=REGION, aws_access_key_id=ACCESS_ID,
+                           aws_secret_access_key=ACCESS_KEY)
+      instances = ec2.instances.filter(
+          Filters=[
+              {
+                  'Name': 'tag:Name',
+                  'Values': [
+                      "instance-state-code",
+                      "tag:k8s.io/role/master"
+                  ]
+              },
+              {
+                  'Name': 'instance-state-code',
+                  'Values': [
+                      "16"
+                  ]
+              }
+          ]
+      )
+      for instance in instances:
+          for tag in instance.tags:
+              print (tag)
+          print ("instance id: ", instance.id)
+    exception:
+      print ("=Exception=")
     print ("Done - waiting for next interval")
     time.sleep(INTERVAL)
